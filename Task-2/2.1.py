32a76775fbf8b9445f403dcafe904c3ba234afed
Aida Aldabergen <153753177+aidaald04@users.noreply.github.com>
2024-10-05 09:01:59 +0500

    2.1.py

--- Task-2/2.1.py
+++ Task-2/2.1.py
@@ -2,7 +2,7 @@ N = int(input("Введите количество школьников: "))
 K = int(input("Введите количество мандаринов: "))
 
 tangerines_per_student = K // N
-remaining_tangerines = K % N
+remainingtangerines = K % N
 
 print(tangerines_per_student)
-print(remaining_tangerines)
+print(remainingtangerines)
